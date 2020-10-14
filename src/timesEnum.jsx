907650983e038734efaa6c79ce0ca7bac86091b8
avichyz <avichyz@gmail.com>
2020-10-14 10:06:39 +0300

    times

---
+++ src/timesEnum.jsx
@@ -0,0 +1,10 @@
+
+const timesEnum = {
+    currentTime: 'currentTime',
+    shabbatEntrence: 'shabbatEntrence',
+    shabbatExit: 'shabbatExit',
+    todaysDate: 'todaysDate',
+    sunRise: 'sunRise'
+}
+
+export default timesEnum;
